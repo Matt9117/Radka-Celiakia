7b9f432f55d802063da70ba779d114cb2568e90c
Matt9117 <mt.banovic@gmail.com>
2025-08-09 09:51:08 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // App.tsx musí byť v priečinku src
-import './index.css';
+import App from './App.tsx';        // <- explicitne s príponou
+import './styles.css';              // <- máš styles.css
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

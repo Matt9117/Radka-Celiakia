62f8e01475c002826a5682c9573b4e6b6d30208f
Matt9117 <mt.banovic@gmail.com>
2025-08-09 21:13:08 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import { defineConfig } from "vite";
 
-import { defineConfig } from 'vite'
 export default defineConfig({
-  server: { host: true },
-  build: { outDir: 'dist' }
-})
+  server: { port: 5173 },
+  build: { outDir: "dist" }
+});
